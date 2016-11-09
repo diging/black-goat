8ef67c8375c885de144eff3b9158f2afe1676515
Erick Peirson <erick.peirson@asu.edu>
2016-11-09 12:40:32 -0500

    static config

--- goat/settings.py
+++ goat/settings.py
@@ -135,7 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+ASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTHORITY_CONFIGURATION_PATH = 'authorities'
 ANONYMOUS_USER_NAME = 'AnonymousUser'
