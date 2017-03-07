8e7744ee759047e044355ab21b9dc71fa57e9f87
Erick Peirson <erick.peirson@asu.edu>
2017-03-07 14:16:21 -0500

    hotfix for config

--- goat/settings.py
+++ goat/settings.py
@@ -71,7 +71,6 @@ AUTHENTICATION_BACKENDS = (
 
 
 MIDDLEWARE_CLASSES = (
-    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
